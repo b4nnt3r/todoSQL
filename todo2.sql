efca67e9c37e03da7bd8740c042d07313003b1f2
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-09 15:41:25 -0400

    sorted incompletes

--- todo2.sql
+++ todo2.sql
@@ -12,7 +12,7 @@ completed_at DATE
 INSERT INTO todo (id, title, details, priority, created_at, completed_at) VALUES ('1', 'Wash Truck', 'Make sure to wax', '1', '2017-08-08', '2017-08-08');
 INSERT INTO todo (id, title, details, priority, created_at) VALUES ('2', 'Dishes', 'Change sign on machine', '2', '2017-08-08');
 INSERT INTO todo (id, title, details, priority, created_at) VALUES ('3', 'Fix AC', 'Florida is hot', '1', '2015-08-08');
-INSERT INTO todo (id, title, details, priority, created_at) VALUES ('4', 'Buy a new t-shirt', 'size large', '3', '2017-08-08');
+INSERT INTO todo (id, title, details, priority, created_at, completed_at) VALUES ('4', 'Buy a new t-shirt', 'size large', '3', '2017-08-05', '2017-08-06');
 INSERT INTO todo (id, title, details, priority, created_at) VALUES ('5', 'Apply for jobs', 'Make some money', '1', '2017-08-08');
 
 
@@ -27,10 +27,10 @@ INSERT INTO todo (id, title, details, priority, created_at) VALUES ('13', 'Hang 
 INSERT INTO todo (id, title, details, priority, created_at) VALUES ('14', 'Write a movie', 'just for fun', '3', '2017-08-09');
 INSERT INTO todo (id, title, details, priority, created_at) VALUES ('15', 'Build a website', 'online', '1', '2017-08-09');
 
-SELECT * FROM todo WHERE priority = 3;
+SELECT * FROM todo WHERE priority = 3 and completed_at is null GROUP BY created_at, todo.id;
 
 SELECT COUNT(title) FROM todo WHERE completed_at is null GROUP BY priority, todo.id;
 
-SELECT COUNT(created_at) FROM todo WHERE created_at > '2017-07-00' GROUP BY priority, todo.id;
+SELECT COUNT(created_at) FROM todo WHERE created_at > '2017-07-10' GROUP BY priority, todo.id;
 
 SELECT *, MAX(created_at) FROM todo WHERE priority = 1 GROUP BY priority, todo.id LIMIT 1;
